4d801ef3013db273cf6e299d9f5b08da7c8f4329
SRIYA REDDY <141334978+hiisriya@users.noreply.github.com>
2023-08-05 18:08:57 +0530

    Update JavaApplication21.java

--- JavaApplication21.java
+++ JavaApplication21.java
@@ -9,6 +9,7 @@ public class JavaApplication21 {
         public static void main(String[] args) {
         
         System.out.println("Hello world");
+                System.out.println("Hii world");
     }
     
 }
